8f7cd526d48d165cb700fccedc09a18aa16acb3c
ruthjoy76 <ruthjoy76@gmail.com>
2023-05-13 10:51:30 +0800

    Update config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mp2-kodementum",
+  base: "/mp2-kodementum/",
   plugins: [react()],
 })
