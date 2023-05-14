08f92c397ab82cd8229820e82da93bf664c93128
ruthjoy76 <ruthjoy76@gmail.com>
2023-05-14 21:48:13 +0800

    Update

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,49 +1,54 @@
 import "./Todo.css";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-
-function Todo() {
+const Todo = () => {
   const [todos, setTodos] = useState([]);
-  const [inputValue, setInputValue] = useState("");
+  const [newTodo, setNewTodo] = useState("");
 
-  const handleFormSubmit = (event) => {
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    if (inputValue.trim() === '') {
-      alert('Please enter a todo item!');
-      return;
-    }
-    
-    setTodos([...todos, inputValue]);
-    setInputValue("");
+    if (!newTodo.trim()) return;
+    setTodos([...todos, { id: Date.now(), text: newTodo }]);
+    setNewTodo("");
   };
 
-  const handleInputChange = (event) => setInputValue(event.target.value);
-
-  const handleTodoDelete = (id) => {
-    setTodos ((todos) => todos.filter((elem, index) => index !==id));
-    alert ('Great job!');
+  const handleDelete = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+    alert("Todo deleted!");
   };
 
   return (
     <div className="todos">
-      <h1 className="question">What is you  goal for today?</h1>
-      <form className="form" onSubmit={handleFormSubmit}>
-        <input type="text" value={inputValue} onChange={handleInputChange} />
+       <h1 className="question">What is you  goal for today?</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter new todo"
+          value={newTodo}
+          onChange={(event) => setNewTodo(event.target.value)}
+        />
         <button type="submit">Add Todo</button>
       </form>
       <ul>
-        {todos.map((todo, id) => (
-          <li key={id}>
-            {todo}
-            <button onClick={() => handleTodoDelete(id)}>x</button>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}{" "}
+            <button onClick={() => handleDelete(todo.id)}>x</button>
           </li>
         ))}
       </ul>
-      
     </div>
   );
-
-  
-}
+};
 
 export default Todo;
